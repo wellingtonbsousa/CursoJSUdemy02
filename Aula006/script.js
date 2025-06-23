3c55b56f8675467e38040ccaf3bd9e98e3e95fa6
Wellington Barbosa e Sousa <wellington.b.sousa2014@gmail.com>
2025-06-23 17:40:32 -0300

    Aula007

---
+++ Aula006/script.js
@@ -0,0 +1,16 @@
+let nome = 'Manuel'
+
+console.log(nome, 'nasceu em 1984.')
+console.log('Em 2000', nome, 'conheceu Maria.')
+console.log(nome, 'casou-se com Maria em 2012.')
+console.log('Maria teve 1 filho com', nome, 'em 2015.')
+console.log('O filho de', nome, 'se chama Eduardo.')
+
+// Não podemos criar variáveis com nomes reservados: ex: let, console, if, etc.
+// Não podemos iniciar o nome da variável com números
+// Não podem ter espaços ou traços
+// Preferencialmente iniciar com letra minúscula
+// Dar nomes significativos
+// Nomes de variáveis são case sensitive
+// Utilizar camelCase
+let nomeCliente
---
+++ Aula007/script.js
@@ -0,0 +1,17 @@
+// Não podemos criar constantes com nomes reservados: ex: let, console, if, etc.
+// Não podemos iniciar o nome da constante com números
+// Não podem ter espaços ou traços
+// Preferencialmente iniciar com letra minúscula
+// Dar nomes significativos
+// Nomes de constantes são case sensitive
+// Utilizar camelCase
+// Não podemos declarar uma constante sem seu valor
+// Não podemos modificar o valor de uma constante
+// const nome = 'João'
+// console.log(nome)
+const primeiroNumero = 5
+const segundoNumero = 2
+const resultado = primeiroNumero * segundoNumero
+console.log(resultado)
+
+
