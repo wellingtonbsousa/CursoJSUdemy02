3e1e5893abeb79b927368cea2faa87c115246236
Wellington Barbosa e Sousa <wellington.b.sousa2014@gmail.com>
2025-06-23 16:44:27 -0300

    Aula004

---
+++ Aula003/index.js
@@ -0,0 +1,15 @@
+// Strings
+console.log('Wellington Sousa')
+console.log("Wellington Sousa")
+console.log(`Wellington Sousa`)
+console.log("Wellington 'Sousa'")
+console.log('Wellington "Sousa"')
+console.log(`'Wellington' "Sousa"`)
+
+// Mumbers
+console.log(35)
+console.log(35, 15.85)
+
+console.log(34, 12.13, 'João')
+
+console.log('Meu nome é "Wellington". Estou aprendendo JavaScript às', 10, 'da manhâ')
---
+++ Aula004/comentarios.js
@@ -0,0 +1,9 @@
+// Isso é um comentário
+console.log('Olá mundo!') // Outro comentário
+
+/*
+Agora é um comentário de bloco
+onde podemos colocar várias linhas
+*/
+
+console.log('Texto 2')
