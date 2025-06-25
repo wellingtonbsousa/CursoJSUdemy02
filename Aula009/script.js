0ef800da27c2a3b2c1f5fa72539b7c86dc508dec
Wellington Barbosa e Sousa <wellington.b.sousa2014@gmail.com>
2025-06-25 16:03:58 -0300

    Aula009

---
+++ Aula009/script.js
@@ -0,0 +1,16 @@
+/*
+// Dados primitivos
+const aluno = 'Wellington' // String
+let num1 = 10              // Number
+let sobreNome              // Undefined -> não aponta para local na memória
+let num2 = null            // Nulo -> não aponta para local na memória
+let aprovado = true         // true ou false (boolean)
+
+console.log(typeof num2, num2)
+*/
+
+let a = 2
+let b = a
+console.log(a, b)
+a = 3
+console.log(a, b)
