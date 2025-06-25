c3d69e71b43eb70f5f9b58b8567647485f0a8db3
Wellington Barbosa e Sousa <wellington.b.sousa2014@gmail.com>
2025-06-25 16:43:33 -0300

    Aula011

---
+++ Aula011/script.js
@@ -0,0 +1,11 @@
+// alert('Nossa mensagem!')
+
+// window.confirm('Deseja mesmo fazer isso?') // Retorna true or false
+
+// window.prompt() // Retorna o conteúdo digitado
+
+const num1 = prompt('Digite um número:')
+const num2 = prompt('Digite outro número:')
+
+const resultado = Number(num1) + Number(num2) 
+alert(`O resultado da soma é ${resultado}`)
