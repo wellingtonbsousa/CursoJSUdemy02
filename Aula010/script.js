63f52b647e226fd2132b85e1fa933b1044e517b2
Wellington Barbosa e Sousa <wellington.b.sousa2014@gmail.com>
2025-06-25 16:23:06 -0300

    Aula010

---
+++ Aula010/script.js
@@ -0,0 +1,43 @@
+// Operadores aritméticos, de atribuição e de incremento
+
+// Aritméticos:
+// + -> Adição e Concatenação
+let num1 = 5
+let num2 = 10
+console.log(num1 + num2)
+let nome1 = 'Wellington '
+let nome2 = 'Sousa'
+console.log(nome1 + nome2)
+// - -> Subtração
+console.log(num1 - num2)
+// * /
+
+// % -> Resto da divisão
+num1 = 10
+num2 = 3
+console.log(num1 % num2)
+
+// Incremento
+// Pré
+let contador = 1
+console.log(++contador)
+// Pós
+contador = 1
+console.log(contador++)
+console.log(contador)
+
+// Decremento
+// --
+
+// Passo
+contador = 1
+contador += 3
+console.log(contador)
+contador = 2
+contador **= 10
+console.log(contador)
+
+// NaN -> Not an Number
+const ze1 = 5
+const ze2 = 'ze'
+console.log(ze1 * ze2)
