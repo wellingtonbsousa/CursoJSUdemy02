226c846f905ba0dbcc601117ca1b018fcda67e30
Wellington Barbosa e Sousa <wellington.b.sousa2014@gmail.com>
2025-06-26 15:57:30 -0300

    Aula015

---
+++ Aula014/script.js
@@ -0,0 +1,16 @@
+let num1 = 10.5879456546876
+let num2 = 2.5
+// num1 = num1.toString()
+// console.log(num1.toString() + num2)
+// console.log(typeof num1)
+
+// console.log(num1.toString(2)) // Converte para número binário
+// console.log(num1.toFixed(4)) // Arredonda para 2 casas decimais
+// console.log(Number.isInteger(num1)) // Verfica se o numero é realmente inteiro
+// let temp = num1 * 'olá'
+// console.log(Number.isNaN(temp)) // Verifica se od resultado é um NaN
+
+num1 = 0.7
+num2 = 0.1
+console.log(num1 + num2)
+
---
+++ Aula015/script.js
@@ -0,0 +1,16 @@
+// Objeto MATH
+
+let num1 = 9.54578  
+let num2 = 0
+
+// num2 = Math.floor(num1)
+// num2 = Math.ceil(num1)
+// num2 = Math.round(num1)
+// console.log(num2)
+// console.log(Math.max(1,4,2,-10,-200, 1000))
+// console.log(Math.min(1,4,2,-10,-200, 1000))
+// num2 = Math.random() // Numero aleatorio entre 0 e 1 (o 1 não aparece)
+// num2 = Math.PI
+// num2 = Math.pow(2, 10) // Eleva 2 à 10 potencia
+num2 = 100 / 0
+console.log(num2)
